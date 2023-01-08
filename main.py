ed736a3b9b5b8c610914148727798c3af1099a7a
L.Bellini <lmbellini@outlook.com>
2023-01-08 20:25:57 -0300

    Páginas dinâmicas

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ def homepage():
 def contacts():
     return render_template("contacts.html")
 
-@app.route("/usuarios/<nome_usuario>")
+@app.route("/usuarios/<nome_usuario>") # página dinâmica de usuários
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
-
