b222b85b47e4e3990900663020778c4792d68b58
L.Bellini <lmbellini@outlook.com>
2023-01-09 10:09:35 -0300

    deploy inicial

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-#criação de página
+
+
+# criação de página
 # route → rota/ caminho/ www.bellini.com/...
 # função → o que vai ser exibido
 # template → códigos html e CSS
@@ -10,14 +12,20 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/contatos")
 def contacts():
     return render_template("contacts.html")
 
-@app.route("/usuarios/<nome_usuario>") # página dinâmica de usuários
+
+@app.route("/usuarios/<nome_usuario>")  # página dinâmica de usuários
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
+
+# deploy do site servidor do heroku
+
